68ac7578cdc2c083dd6af698ab677091c2a5c334
HY <hy123@123.com>
2022-08-25 16:38:21 +0800

    master  commit2

--- src/main/java/gitTest/Main.java
+++ src/main/java/gitTest/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git hot-fix");
+        System.out.println("hello git master");
     }
 }
