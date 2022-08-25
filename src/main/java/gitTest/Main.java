c46a3eb9da075ab40e7d4f751eeb86affa1ccad1
xpzdbb <62935856+xpzdbb@users.noreply.github.com>
2022-08-25 16:52:41 +0800

    pull commit

--- src/main/java/gitTest/Main.java
+++ src/main/java/gitTest/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("hello git master");
         System.out.println("hello git hot-fix2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
